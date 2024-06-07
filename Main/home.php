484c1afc256847d96cd7b753817624ee4907c18b
sPyKeRT1 <franciscortc@gmail.com>
2024-06-06 22:12:57 -0400

    qshushawnnnporquenofunciona

--- Main/home.php
+++ Main/home.php
@@ -1,5 +1,4 @@
 <?php
-    include('./header.php');
     include('./config/db.php');
 
 
@@ -7,7 +6,6 @@
 
   if (isset($_SESSION['nombre_usuario'])) {
     $nombreUsuario = $_SESSION['nombre_usuario'];
-    echo "Bienvenido, " . htmlspecialchars($nombreUsuario) . "!";
   } else {
     header("Location: ./index.php");
     exit();
@@ -15,28 +13,16 @@
 ?>
 
 <!DOCTYPE html>
-<html data-theme="light" lang="en" style= "background-color:#fff1ee" >
+<html data-theme="light" lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>SRH</title>
     <link rel="icon" type="image/x-icon" href="./assets/images/icons8-caduceus-16.png"/>
     <link rel="stylesheet" href="./assets/css/style.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.indigo.min.css"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.colors.min.css"/>
   </head>
 
   <body class="body-home">
-    <div class="contenedor">
-      <div class="columna">
-        <h1>A</h1>
-      </div>
-      <div class="columna">
-        <h1>A</h1>
-      </div>
-      <div class="columna">
-        <h1>A</h1>
-      </div>
-    </div>
+
   </body>
 </html>
